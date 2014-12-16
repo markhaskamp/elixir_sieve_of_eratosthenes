727f3f8c0f61e0e99896c1f02656e3dbf0123cf3
Mark Haskamp <mark.haskamp@pinnsg.com>
2014-12-15 19:28:44 -0500

    resolve merge conflict

--- lib/sieve.ex
+++ lib/sieve.ex
@@ -4,14 +4,16 @@ defmodule Sieve do
     _primes([], 
             :math.sqrt(n), 
             2..n |> Enum.into([]))
-
   end
 
-  def _primes(primes, n, list) when hd(list) >= n, do: Enum.reverse(primes) ++ list
-  def _primes(primes, n, [first | tail]) do
-    _primes([first | primes],
-            n, 
-            Enum.filter(tl(tail), &(rem(&1, first) != 0)))
+  def _primes(primes, n, list) do
+    first = hd(list)
+    cond do
+      first >= n -> Enum.reverse(primes) ++ list
+      true -> _primes([first | primes],
+                      n, 
+                      Enum.filter(tl(list), &(rem(&1, first) != 0)))
+    end
 
   end
 end
